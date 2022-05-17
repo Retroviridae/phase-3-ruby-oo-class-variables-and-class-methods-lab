9f548d697f072579581674d728df52eabfce4c12
Alexander Schuessler <schuessler.alex@gmail.com>
2022-05-17 16:00:17 -0500

    commit tree a28ae8a7bac175552797df17057e171da7115784

---
+++ lib/song.rb
@@ -0,0 +1,24 @@
+class Song
+    @@count = 0
+    @@artists = []
+    @@genres= []
+
+    def initialize(name,artist,genre)
+        @name = name
+        @artist = artist
+        @genre = genre
+        count += 1
+        @@artists << artist
+        @@genres << genre
+    end
+    def self.count
+        @@count
+    end
+    def self.artists
+        @@artists
+    end
+    def self.genre
+        @@genres
+    end
+    
+end
